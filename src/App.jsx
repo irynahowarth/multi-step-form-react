fc10572ee5d5cb4d2d2c140424df942f273e0362
Iryna Howarth <irynahowarth@gmail.com>
2024-01-24 22:46:31 +0000

    [style]:add styles to main elements and form:Step 1

--- src/App.jsx
+++ src/App.jsx
@@ -29,131 +29,162 @@ const PLANS = [
 
 function App() {
   const id = React.useId();
+  const [step, setStep] = React.useState(1);
   return (
     <form>
-      <ol>
-        {STEPS.map(({ value, label }) => (
-          <li key={value}>
-            <button type="button">
-              <span>{value}</span>
+      <div className="form-wrapper">
+        <header className="form-header">
+          <ol>
+            {STEPS.map(({ value, label }) => (
+              <li key={value} className={value === 1 ? "active" : undefined}>
+                <div className="marker-circle">{value}</div>
+                <div>
+                  <span>{`step ${value}`}</span>
+                  <div>{label}</div>
+                </div>
+              </li>
+            ))}
+          </ol>
+        </header>
+        <main>
+          {step === 1 && (
+            <section className="section-wrapper">
               <div>
-                <div>step {value}</div>
-                <div>{label}</div>
+                <h1>Personal info</h1>
+                <p>
+                  Please provide your name, email address, and phone number.
+                </p>
               </div>
-            </button>
-          </li>
-        ))}
-      </ol>
-      <section>
-        <h1>Personal info</h1>
-        <p>Please provide your name, email address, and phone number.</p>
-        <label htmlFor={`name-${id}`}>Name</label>
-        <input type="text" id={`name-${id}`} required />
-        <label htmlFor={`email-${id}`}>Email address</label>
-        <input type="email" id={`email-${id}`} required />
-        <label htmlFor={`phone-${id}`}>Phone number</label>
-        <input type="text" id={`phone-${id}`} required />
-      </section>
-      <section>
-        <h1>Select your plan</h1>
-        <p>You have the option of monthly or yearly billing.</p>
-        <ul>
-          <li>
-            <input type="radio" />
-            <label htmlFor="">
-              <h4>Arcade</h4>
-              <span>$90/year</span>
-              <span>2 months free</span>
-            </label>
-          </li>
-          <li>
-            <input type="radio" />
-            <label htmlFor="">
-              <h4>Advanced</h4>
-              <span>$90/year</span>
-              <span>2 months free</span>
-            </label>
-          </li>
-          <li>
-            <input type="radio" />
-            <label htmlFor="">
-              <h4>Pro</h4>
-              <span>$90/year</span>
-              <span>2 months free</span>
-            </label>
-          </li>
-        </ul>
-        <div>
-          <span>Monthly</span>
-          <button type="button">
-            <span></span>
-          </button>
-          <span>Yearly</span>
-        </div>
-      </section>
-      <section>
-        <h1>Pick Add-ons</h1>
-        <p>Add-ons help enhance your gaming experience.</p>
-        <div>
-          <input type="checkbox" />
-          <label>
-            <div>
-              <h4>Online service</h4>
-              <p>Access to multiplayer games</p>
-            </div>
-            <div>+$1/mo</div>
-          </label>
-        </div>
-        <div>
-          <input type="checkbox" />
-          <label>
-            <div>
-              <h4>Large storage</h4>
-              <p>Extra 1TB of cloud save</p>
-            </div>
-            <div>+$2/mo</div>
-          </label>
-        </div>
-        <div>
-          <input type="checkbox" />
-          <label>
-            <div>
-              <h4>Customizable Profile</h4>
-              <p>Custom theme on your profile</p>
-            </div>
-            <div>+$2/mo</div>
-          </label>
-        </div>
-      </section>
-      <section>
-        <h1>Finishing up</h1>
-        <p>Double-check everything looks OK before confirming.</p>
-        <dl>
-          <dt>
-            <span>Arcade(Monthly)</span>
-            <a type="button">Change</a>
-          </dt>
-          <dd>$9/mo</dd>
-          <dt>Online service</dt>
-          <dd>+$1/mo</dd>
-          <dt>Larger storage</dt>
-          <dd>+$2/mo</dd>
-          <dt>Total(per month)</dt>
-          <dd>+$12/mo</dd>
-        </dl>
-      </section>
-      <section>
-        <button type="button">Go Back</button>
-        <button type="button">Next Step</button>
-      </section>
-      <section>
-        <h1>Thank you!</h1>
-        <p>
-          Thanks for confirming your subscription! We hope you have fun using
-          our platform. If you ever need support, please feel free to email us
-          at support@loremgaming.com
-        </p>
-      </section>
+              <div>
+                <label htmlFor={`name-${id}`}>Name</label>
+                <input type="text" id={`name-${id}`} required />
+              </div>
+              <div>
+                <label htmlFor={`email-${id}`}>Email address</label>
+                <input type="email" id={`email-${id}`} required />
+              </div>
+              <div>
+                <label htmlFor={`phone-${id}`}>Phone number</label>
+                <input type="text" id={`phone-${id}`} required />
+              </div>
+            </section>
+          )}
+          {step === 2 && (
+            <section className="section-wrapper">
+              <h1>Select your plan</h1>
+              <p>You have the option of monthly or yearly billing.</p>
+              <ul>
+                <li>
+                  <input type="radio" />
+                  <label htmlFor="">
+                    <h4>Arcade</h4>
+                    <span>$90/year</span>
+                    <span>2 months free</span>
+                  </label>
+                </li>
+                <li>
+                  <input type="radio" />
+                  <label htmlFor="">
+                    <h4>Advanced</h4>
+                    <span>$90/year</span>
+                    <span>2 months free</span>
+                  </label>
+                </li>
+                <li>
+                  <input type="radio" />
+                  <label htmlFor="">
+                    <h4>Pro</h4>
+                    <span>$90/year</span>
+                    <span>2 months free</span>
+                  </label>
+                </li>
+              </ul>
+              <div>
+                <span>Monthly</span>
+                <button type="button">
+                  <span></span>
+                </button>
+                <span>Yearly</span>
+              </div>
+            </section>
+          )}
+          {step === 3 && (
+            <section className="section-wrapper">
+              <h1>Pick Add-ons</h1>
+              <p>Add-ons help enhance your gaming experience.</p>
+              <div>
+                <input type="checkbox" />
+                <label>
+                  <div>
+                    <h4>Online service</h4>
+                    <p>Access to multiplayer games</p>
+                  </div>
+                  <div>+$1/mo</div>
+                </label>
+              </div>
+              <div>
+                <input type="checkbox" />
+                <label>
+                  <div>
+                    <h4>Large storage</h4>
+                    <p>Extra 1TB of cloud save</p>
+                  </div>
+                  <div>+$2/mo</div>
+                </label>
+              </div>
+              <div>
+                <input type="checkbox" />
+                <label>
+                  <div>
+                    <h4>Customizable Profile</h4>
+                    <p>Custom theme on your profile</p>
+                  </div>
+                  <div>+$2/mo</div>
+                </label>
+              </div>
+            </section>
+          )}
+          {step === 4 && (
+            <section className="section-wrapper">
+              <h1>Finishing up</h1>
+              <p>Double-check everything looks OK before confirming.</p>
+              <dl>
+                <dt>
+                  <span>Arcade(Monthly)</span>
+                  <a type="button">Change</a>
+                </dt>
+                <dd>$9/mo</dd>
+                <dt>Online service</dt>
+                <dd>+$1/mo</dd>
+                <dt>Larger storage</dt>
+                <dd>+$2/mo</dd>
+                <dt>Total(per month)</dt>
+                <dd>+$12/mo</dd>
+              </dl>
+            </section>
+          )}
+          {step !== 5 && (
+            <section className="section-wrapper actions">
+              <button type="button" className="btn">
+                Go Back
+              </button>
+              <button type="button" className="btn primary">
+                Next Step
+              </button>
+            </section>
+          )}
+          {step === 5 && (
+            <section className="section-wrapper">
+              <h1>Thank you!</h1>
+              <p>
+                Thanks for confirming your subscription! We hope you have fun
+                using our platform. If you ever need support, please feel free
+                to email us at support@loremgaming.com
+              </p>
+            </section>
+          )}
+        </main>
+      </div>
     </form>
   );
 }
